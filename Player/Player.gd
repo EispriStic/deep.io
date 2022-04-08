ab215c25d35818bf5cb778d63f0f6d5ea45c0f5e
Noname <63104855+IK-NoName@users.noreply.github.com>
2022-04-08 09:10:23 +0200

    directory management
    j'ai vite fais renommer et delete deux trois truc pas utile (genr loris)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -6,11 +6,46 @@ extends Node2D
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+var velocity = Vector2()
+var can_fire = true
+
+var bullet = preload("res://Balles/Balle_classic.tscn")
+
+
+func _process(delta):
+	var sante = Tank.pv
+	if Tank.pv <= 0 :
+		get_tree().change_scene("res://Menu/Menu.tscn")
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("fire") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $Bullet_point.get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(), Vector2(Tank.min_sspeed, 0 ).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(Tank.reload),"timeout")
+		can_fire = true
+		Tank.pv -= 10
+		
+		
+		
+		
+func _physics_process(delta):
+	velocity.x = (int(Input.is_action_pressed('right')) - int(Input.is_action_pressed('left'))) *Tank.min_speed
+	velocity.y = (int(Input.is_action_pressed('down')) - int(Input.is_action_pressed('up'))) *Tank.min_speed
+	velocity.normalized()
+	move_and_slide(velocity)
+#	var direction = Vector2()
+#	if Input.is_action_pressed('right') :
+#		direction += Vector2(1,0)
+#	if Input.is_action_pressed("left") :
+#		direction += Vector2(-1,0)
+#	if Input.is_action_pressed('up') :
+#		direction += Vector2(0,-1)
+#	if Input.is_action_pressed('down') :
+#		direction += Vector2(0,1)
+#	move_and_slide(direction * speed)
