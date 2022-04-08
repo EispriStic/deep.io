055221520d13d86eecb6c18dd6fec68943ee21ce
Paul.Schultz <polo.schultz.01@gmail.com>
2022-04-08 21:31:23 +0200

    les BLOCS!!!!!

--- Main/Main.gd
+++ Main/Main.gd
@@ -19,26 +19,28 @@ var timer_h = 5.0
 
 func _process(delta):
 	if time_c > timer_c:
-		var carre = load("res://Props/Carre.tscn").instance()
-		$SpawnProps.add_child(carre)
+		for i in range(10):
+			var carre = load("res://Props/Carre.tscn").instance()
+			$SpawnProps.add_child(carre)
 		time_c = 0.0
-	timer_c += delta
+	time_c += delta
 	
 	if time_t > timer_t:
-		var triangle = load("res://Props/Triangle.tscn").instance()
-		$SpawnProps.add_child(triangle)
+		for i in range(10):
+			var triangle = load("res://Props/Triangle.tscn").instance()
+			$SpawnProps.add_child(triangle)
 		time_t = 0.0
-	timer_t += delta
+	time_t += delta
 	
 	if time_p > timer_p:
 		var pentagone = load("res://Props/Pentagone.tscn").instance()
 		$SpawnProps.add_child(pentagone)
 		time_p = 0.0
-	timer_p += delta
+	time_p += delta
 	
 	if time_h > timer_h:
 		var hexagone = load("res://Props/Hexagone.tscn").instance()
 		$SpawnProps.add_child(hexagone)
 		time_h = 0.0
-	timer_h += delta
+	time_h += delta
 
